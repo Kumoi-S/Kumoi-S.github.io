05428dc5057b805bb60b33250dd0b0b396360b4c
smy 4070laptop <kumoishizuka@gmail.com>
2025-02-06 19:38:51 +0800

    temp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kumoi-s.github.io',
-  base: '/test',
+  // base: '/test',
   trailingSlash: "never",
   prefetch: {
     prefetchAll: true,
